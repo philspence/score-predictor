7e5efd9479a52b97f51b777b130f2e1ce7625d83
philspence <philspence91@gmail.com>
2020-06-19 15:11:40 +0100

    model trained with 27 seasons from 4 leagues

--- form-calc.py
+++ form-calc.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+import os
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
-
-data = pd.read_csv('merged_noodds.csv', header=0)
 
 
 def get_mv_avg(df, key, value, win, newcol):
@@ -13,31 +13,39 @@ def get_mv_avg(df, key, value, win, newcol):
     df[newcol] = moving_avg.set_index('level_1')[value]
     return df
 
-w = 3
-# HGFAvg = Home Goals For Average, S = Shots
-data = get_mv_avg(data, 'HomeTeam', 'FTHG', w, 'HGFAvg')
-data = get_mv_avg(data, 'HomeTeam', 'FTAG', w, 'HGAAvg')
-data = get_mv_avg(data, 'HomeTeam', 'HS', w, 'HSFAvg')
-data = get_mv_avg(data, 'HomeTeam', 'AS', w, 'HSAAvg')
 
-data = get_mv_avg(data, 'AwayTeam', 'FTAG', w, 'AGFAvg')
-data = get_mv_avg(data, 'AwayTeam', 'FTHG', w, 'AGAAvg')
-data = get_mv_avg(data, 'AwayTeam', 'AS', w, 'ASFAvg')
-data = get_mv_avg(data, 'AwayTeam', 'HS', w, 'ASAAvg')
+def calc_form(infile, w):
+    data = pd.read_csv(f'{infile}.csv', header=0)
+    print('Imported CSV')
+    data = data[['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG']]
+    data['Date'] = pd.to_datetime(data['Date'], infer_datetime_format=True)
+    #data = data.loc[data.Date > '01/01/2000']
+    data = data.dropna()
+    print('Removed excess data')
+    
+    # HGFAvg = Home Goals For Average, S = Shots
+    data = get_mv_avg(data, 'HomeTeam', 'FTHG', w, 'HGFAvg')
+    data = get_mv_avg(data, 'HomeTeam', 'FTAG', w, 'HGAAvg')
+    #data = get_mv_avg(data, 'HomeTeam', 'HS', w, 'HSFAvg')
+    #data = get_mv_avg(data, 'HomeTeam', 'AS', w, 'HSAAvg')
 
-# moving_avg = data.groupby('HomeTeam')['FTHG'].rolling(window=10).mean().reset_index()
-# data['HFAvg'] = moving_avg.set_index('level_1')['FTHG']
-# moving_avg = data.groupby('HomeTeam')['FTAG'].rolling(window=10).mean().reset_index()
-# data['HAAvg'] = moving_avg.set_index('level_1')['FTAG']
-#
-#
-#
-# moving_avg = data.groupby('AwayTeam')['FTAG'].rolling(window=10).mean().reset_index()
-# data['AFAvg'] = moving_avg.set_index('level_1')['FTAG']
-# moving_avg = data.groupby('AwayTeam')['FTHG'].rolling(window=10).mean().reset_index()
-# data['AAAvg'] = moving_avg.set_index('level_1')['FTHG']
+    data = get_mv_avg(data, 'AwayTeam', 'FTAG', w, 'AGFAvg')
+    data = get_mv_avg(data, 'AwayTeam', 'FTHG', w, 'AGAAvg')
+    #data = get_mv_avg(data, 'AwayTeam', 'AS', w, 'ASFAvg')
+    #data = get_mv_avg(data, 'AwayTeam', 'HS', w, 'ASAAvg')
+    data = data.dropna(subset=['HGFAvg', 'HGAAvg', 'AGFAvg', 'AGAAvg'])
+    data.to_csv(f'{infile}-avgs.csv')
+    return data
+    
+    
+def load_form(infile):
+    data = pd.read_csv(f'{infile}.csv', header=0)
+    return data
+# removes the first 4 games for each team (i.e. no form to calculate with)
 
-data = data.dropna()
+
+#data = calc_form('merged-all', 3)
+data = load_form('merged-all-avgs')
 
 #Xh = data[['HGFAvg', 'AGAAvg', 'HSFAvg', 'ASAAvg']].to_numpy()
 #Xa = data[['AGFAvg', 'HGAAvg', 'ASFAvg', 'HSAAvg']].to_numpy()
@@ -48,7 +56,7 @@ Xa = data[['AGFAvg', 'HGAAvg']].to_numpy()
 X = np.concatenate((Xh, Xa))
 
 yh = data['FTHG'].to_numpy()
-ya = data['HTAG'].to_numpy()
+ya = data['FTAG'].to_numpy()
 y = np.concatenate((yh, ya))
 
 #scaler = MinMaxScaler()
@@ -57,17 +65,29 @@ y = np.concatenate((yh, ya))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
+def build_model():
+    opt = tf.keras.optimizers.RMSprop(momentum=0.05)
+    model = Sequential()
+    model.add(Dense(4, activation='relu', input_dim=2))
+    #model.add(Dropout(0.33))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(4, activation='relu'))
+    #model.add(Dense(2, activation='relu'))
+    #model.add(Dense(4, activation='relu'))
+    #model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='selu'))
+    model.compile(loss='mae', optimizer='rmsprop',  metrics=['mae'])
+    return model
 
-model = Sequential()
-model.add(Dense(4, activation='relu', input_dim=2))
-#model.add(Dropout(0.33))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(4, activation='relu'))
-#model.add(Dense(8, activation='relu'))
-#model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='selu'))
-model.compile(loss='mae', optimizer='rmsprop',  metrics=['mae'])
-model.fit(X_train, y_train, epochs=10, verbose=1, validation_data=(X_test, y_test))
+
+if os.path.isfile('avgG-model-06'):
+    model = load_model('avgG-model-06')
+else:
+    model = build_model()
+    model.fit(X_train, y_train, epochs=5, verbose=1, validation_data=(X_test, y_test))
+    #model.save('avgG-model')
 y_pred = model.predict(X_test)
-pred_real = list(zip(y_pred, y_test))
-print(pred_real[0:20])
+y_test_np = np.array([y_test])
+y_test_np = np.reshape(y_test_np, (-1, 1))
+pred_real = np.append(y_pred, y_test_np, axis=1)
+print(np.round(pred_real, 1)[0:20])
