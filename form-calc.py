4377b09f14d4bb60674a72d1473b200feab58f88
philspence <philspence91@gmail.com>
2020-06-20 15:37:35 +0100

    added some historic predictions

--- form-calc.py
+++ form-calc.py
@@ -43,30 +43,28 @@ def load_form(infile):
     return data
 # removes the first 4 games for each team (i.e. no form to calculate with)
 
+def get_training_data(infile, load=True):
+    if load is True:
+        data = load_form(f'{infile}-avgs')
+    else:
+        data = calc_form(infile, 3)
+    
+    #Xh = data[['HGFAvg', 'AGAAvg', 'HSFAvg', 'ASAAvg']].to_numpy()
+    #Xa = data[['AGFAvg', 'HGAAvg', 'ASFAvg', 'HSAAvg']].to_numpy()
+    Xh = data[['HGFAvg', 'AGAAvg']].to_numpy()
+    Xa = data[['AGFAvg', 'HGAAvg']].to_numpy()
+    X = np.concatenate((Xh, Xa))
 
-#data = calc_form('merged-all', 3)
-data = load_form('merged-all-avgs')
-
-#Xh = data[['HGFAvg', 'AGAAvg', 'HSFAvg', 'ASAAvg']].to_numpy()
-#Xa = data[['AGFAvg', 'HGAAvg', 'ASFAvg', 'HSAAvg']].to_numpy()
-Xh = data[['HGFAvg', 'AGAAvg']].to_numpy()
-Xa = data[['AGFAvg', 'HGAAvg']].to_numpy()
-#Xh = data['HGFAvg'].to_numpy()
-#Xa = data['AGFAvg'].to_numpy()
-X = np.concatenate((Xh, Xa))
-
-yh = data['FTHG'].to_numpy()
-ya = data['FTAG'].to_numpy()
-y = np.concatenate((yh, ya))
-
-#scaler = MinMaxScaler()
-#scaler = scaler.fit(X)
-#X = scaler.transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    yh = data['FTHG'].to_numpy()
+    ya = data['FTAG'].to_numpy()
+    y = np.concatenate((yh, ya))
+    #scaler = MinMaxScaler()
+    #scaler = scaler.fit(X)
+    #X = scaler.transform(X)
+    return train_test_split(X, y, test_size=0.33, random_state=42)
 
 def build_model():
-    opt = tf.keras.optimizers.RMSprop(momentum=0.05)
+    opt = tf.keras.optimizers.RMSprop(momentum=0.15)
     model = Sequential()
     model.add(Dense(4, activation='relu', input_dim=2))
     #model.add(Dropout(0.33))
@@ -80,13 +78,38 @@ def build_model():
     return model
 
 
+def load_data(infile):
+    data = pd.read_csv(f'{infile}.csv', header=0)
+    Xh = data[['HGFAvg', 'AGAAvg']].to_numpy()
+    Xa = data[['AGFAvg', 'HGAAvg']].to_numpy()
+    X = np.concatenate((Xh, Xa))
+
+    yh = data['FTHG'].to_numpy()
+    ya = data['FTAG'].to_numpy()
+    y = np.concatenate((yh, ya))
+    
+    return X, y
+
+
+train = False
+if train is True:
+    X_train, X_test, y_train, y_test = get_training_data('merged-all-avgs')
+else:
+    data = calc_form('merged-EPL', 3)
+    X, y = load_data('merged-EPL-avgs')
 if os.path.isfile('avgG-model-06'):
     model = load_model('avgG-model-06')
 else:
     model = build_model()
     model.fit(X_train, y_train, epochs=5, verbose=1, validation_data=(X_test, y_test))
-    #model.save('avgG-model')
-y_pred = model.predict(X_test)
+    model.save('avgG-model')
+
+    
+y_pred = model.predict(X)
+yH, ya = np.split(y_pred, 2)[0], np.split(y_pred, 2)[1]
+data['PredHG'],  data['PredAG'] = [np.round(yH, 1), np.round(ya, 1)]
+data.to_csv('merged-EPL-preds.csv')
+exit()
 y_test_np = np.array([y_test])
 y_test_np = np.reshape(y_test_np, (-1, 1))
 pred_real = np.append(y_pred, y_test_np, axis=1)
